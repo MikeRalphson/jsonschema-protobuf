e4c281e2e2ef95bbf8d5df4a92e904a6222ce5bf
Mike Ralphson <mike.ralphson@gmail.com>
2021-03-03 03:24:30 +0000

    fix: changes from working on asyncapi schema, $refs etc

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const yaml = require('yaml');
+const { jptr } = require('reftools/lib/jptr');
+
 var protobuf = require('protocol-buffers-schema')
 var mappings = {
   'array': 'repeated',
-//  'object': 'message',
-  'integer': 'int32',
-  'number': 'int32',
+  'object': 'message',
+  'integer': 'int64',
+  'number': 'float',
   'string': 'string',
   'boolean': 'bool'
 }
+var base;
 
 var protoBufRoot = {
-  syntax: 2,
+  syntax: 3,
   package: null,
   enums: [],
   messages: []
@@ -17,12 +21,13 @@ var protoBufRoot = {
 
 module.exports = function (schema) {
   protoBufRoot = {
-    syntax: 2,
+    syntax: 3,
     package: null,
     enums: [],
     messages: []
   };
-  if (typeof schema === 'string') schema = JSON.parse(schema)
+  if (typeof schema === 'string') schema = yaml.parse(schema)
+  base = schema;
   result = protoBufRoot;
 
   if (schema.type === 'object') {
@@ -31,9 +36,14 @@ module.exports = function (schema) {
   return protobuf.stringify(result)
 }
 
-function Message (schema) {
+function Message (schema, parentProp) {
+
+  if (schema.$ref) { // follow $refs
+    schema = jptr(base,schema.$ref)
+  }
+
   var message = {
-    name: schema.name,
+    name: schema.name||parentProp||schema.title,
     enums: [],
     messages: [],
     fields: []
@@ -42,13 +52,33 @@ function Message (schema) {
   var tag = 1
   for (var key in schema.properties) {
     var field = schema.properties[key]
+    if (field.$ref) { // follow $refs
+      field = jptr(base,field.$ref)
+    }
     field.name = key
     if (field.type === 'object') {
       field.type = field.name + 'Type';
       message.fields.push(Field(field, tag, message))
       field.name = field.type;
-      message.messages.push(Message(field))
-    }else{
+      message.messages.push(Message(field,key))
+    } else {
+      message.fields.push(Field(field, tag, message))
+    }
+    tag += 1
+  }
+
+  if (typeof schema.additionalProperties === 'object') {
+    var field = schema.additionalProperties
+    if (field.$ref) { // follow $refs
+      field = jptr(base,field.$ref)
+    }
+    if (field.type === 'object') {
+      field.name = 'additionalProperties';
+      field.type = field.name + 'Type';
+      message.fields.push(Field(field, tag, message))
+      field.name = field.type;
+      message.messages.push(Message(field,key))
+    } else {
       message.fields.push(Field(field, tag, message))
     }
     tag += 1
@@ -66,24 +96,27 @@ function Message (schema) {
 }
 
 function Field(field, tag, message) {
-  var type = mappings[field.type] || field.type
+  var type = mappings[field.type] || field.type || 'bytes'
   var repeated = false
 
   if (field.type === 'array') {
     repeated = true
     if (field.items.type === 'object') {
+      if (field.items.$ref) { // follow $refs
+        field.items = jptr(base,field.items.$ref)
+      }
       field.items.name = field.name;
-      protoBufRoot.messages.push(Message(field.items))
+      protoBufRoot.messages.push(Message(field.items,field.name))
       type = field.name
     } else {
       type = field.items.type
     }    
-  }else if(field.type === 'string' && field.enum){
+  } else if (field.type === 'string' && field.enum){
     type = field.name + "Enum";
     message.enums.push(Enum(field))
   }
 
-
+  //console.assert(typeof field.name !== 'undefined');
   return {
     name: field.name,
     type: type,
